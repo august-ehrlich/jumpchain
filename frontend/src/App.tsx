98e511f65939969f5bf2bc2f045af581e2f97186
August Ehrlich <augustehrlich04@gmail.com>
2026-07-15 20:23:30 -0700

    Uses ShadCN UI styles on main page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,22 @@
+import React, { useState } from "react"
+import Header from "./layout/Header"
+import Footer from "./layout/Footer"
+import { Button } from "./components/ui/button"
 
-const App = () => <h1>Vite is running in Docker with TypeScript!</h1>
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <React.Fragment>
+        <Header/>
+            <div className="p-4">
+                <h1>Vite is running in Docker with TypeScript!</h1>
+                <p>Count: {count}</p>
+                <Button onClick={() => setCount(count + 1)}>Increment</Button>
+            </div>
+        <Footer/>
+    </React.Fragment>
+  )
+}
 
 export default App
--- frontend/src/Main.tsx
+++ frontend/src/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.js'
+import './styles/globals.css'
 
 // The '!' asserts that the element will not be null
 ReactDOM.createRoot(document.getElementById('root')!).render(
